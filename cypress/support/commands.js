670e0faac9dad3fecc3f342eaa9b1e9f2c264171
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-08-29 16:09:39 -0300

    feat: adicionar comandos de login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,6 +3,23 @@
 // create various custom commands and overwrite
 // existing commands.
 //
+
+Cypress.Commands.add('sessionLogin', () => {
+	cy.visit('/login')
+	cy.get('#email').should('be.visible')
+})
+
+Cypress.Commands.add('userLogin', (email, senha) => {
+  cy.get('#email').should('be.visible').clear()
+  if (email) cy.get('#email').type(email)
+
+  cy.get('#senha').should('be.visible').clear()
+  if (senha) cy.get('#senha').type(senha) 
+
+  cy.get('.btn-primary').should('be.visible').click()
+})
+
+
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
